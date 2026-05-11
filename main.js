9ddedd3c8265d51ee8029d0ed8da6607a8ff4619
LeoPorcellatti <leoporcellatti@live.com.ar>
2026-05-11 17:46:14 -0300

    preguntas ok

--- main.js
+++ main.js
@@ -1,164 +1,261 @@
 const data = [
-  // CIENCIA
+  // POKEMON
   {
-    pregunta: "¿Cuál es el planeta más grande del sistema solar?",
-    respuesta: "Júpiter",
-    opciones: ["Saturno", "Júpiter", "Neptuno", "Tierra"],
+    pregunta: "¿Cuál de estas regiones NO existen en el universo Pokémon?",
+    respuesta: "Sansfer",
+    opciones: ["Alola", "Sansfer", "Hisui", "Kalos"],
   },
   {
-    pregunta: "¿Cuántos huesos tiene el cuerpo humano adulto?",
-    respuesta: "206",
-    opciones: ["186", "206", "226", "256"],
+    pregunta: "¿Cuántos pokémon hubo en la primer generación?",
+    respuesta: "151",
+    opciones: ["151", "150", "152", "149"],
   },
   {
-    pregunta: "¿Cuál es el gas más abundante en la atmósfera terrestre?",
-    respuesta: "Nitrógeno",
-    opciones: ["Oxígeno", "Nitrógeno", "Argón", "Dióxido de carbono"],
+    pregunta: "¿Cuál es la máscota de Pokémon?",
+    respuesta: "Pikachu",
+    opciones: ["Eevee", "Charizard", "Mewtwo", "Pikachu"],
   },
   {
-    pregunta: "¿Cuál es el elemento químico de la sangre que transporta oxígeno?",
-    respuesta: "Hierro",
-    opciones: ["Calcio", "Hierro", "Magnesio", "Potasio"],
+    pregunta: "¿De qué tipo es el pokemón tortuga Squirtle?",
+    respuesta: "Agua",
+    opciones: ["Tierra", "Planta", "Agua", "Acero"],
   },
   {
-    pregunta: "¿Cuántos dedos tienen los humanos en total?",
-    respuesta: "10",
-    opciones: ["8", "10", "12", "20"],
+    pregunta: "¿Cuál de estos tipos no existe en el universo Pokémon?",
+    respuesta: "Luz",
+    opciones: ["Luz", "Siniestro", "Hada", "Dragón"],
+  },
+  {
+    pregunta: "¿En que año se lanzó el primer juego de Pokémon?",
+    respuesta: "1996",
+    opciones: ["1994", "1996", "1998", "2000"],
+  },
+  {
+    pregunta: "¿Como se llama el protagonista del anime Pokémon?",
+    respuesta: "Ash",
+    opciones: ["Gary", "Brock", "Ash", "Giovani"],
+  },
+  {
+    pregunta: "¿Cuál de estos Pokémon no es un inicial?",
+    respuesta: "Luxio",
+    opciones: ["Rowlet", "Litten", "Popplio", "Luxio"],
+  },  
+  {
+    pregunta: "¿Cuál de estos Pokémon es el primer pokémon?",
+    respuesta: "Bulbasaur",
+    opciones: ["Bulbasaur", "Mew", "Rhydon", "Arceus"],
+  },
+  {
+    pregunta: "¿Cuál de estos Pokémon es el DIOS pokémon?",
+    respuesta: "Arceus",
+    opciones: ["Magikarp", "Psyduck", "Arceus", "Maractus"],
   },
 
-  // HISTORIA ARGENTINA
+  // RESIDENT EVIL
   {
-    pregunta: "¿En qué año Argentina declara su independencia?",
-    respuesta: "1816",
-    opciones: ["1810", "1816", "1820", "1826"],
+    pregunta: "¿Quienes los protagonistas de Resident Evil 1?",
+    respuesta: "Jill - Chris",
+    opciones: ["Rebecca y Billy", "Jill y Chris", "Leon y Claire", "Jill y Carlos"],
   },
   {
-    pregunta: "¿Quién fue el libertador de América del Sur?",
-    respuesta: "José de San Martín",
-    opciones: ["Simón Bolívar", "José de San Martín", "Manuel Belgrano", "Juan Manuel de Rosas"],
+    pregunta: "¿Quién fue el infiltrado en los STARS (spoiler de un juego de más de 20 años)?",
+    respuesta: "Albert Wesker",
+    opciones: ["Albert Wesker", "Barry Burton", "Ada Wong", "Billy Cohen"],
   },
   {
-    pregunta: "¿Cuál es la capital de Argentina?",
+    pregunta: "¿Cuál es la primer jefe final de los Juegos de Resident Evil?",
+    respuesta: "Tyrant",
+    opciones: ["Nemesis", "Gigante", "Tyrant", "Hunk"],
+  },
+  {
+    pregunta: "¿Cómo se llama la ciudad de la saga Resident Evil?",
+    respuesta: "Raccon City",
+    opciones: ["Umbrella City", "Arklay City", "Turtles Crty", "Raccoon City"],
+  },
+  {
+    pregunta: "¿En qué año se lanzó el primer juego de Resident Evil?",
+    respuesta: "1996",
+    opciones: ["1996", "1995", "1994", "1993"],
+  },
+  {
+    pregunta: "¿En cuál país no toma lugar ningún juego de Resident Evil?",
+    respuesta: "Brasil",
+    opciones: ["Estados Unidos", "Brasil", "China", "España"],
+  },
+  {
+    pregunta: "¿Cuál era la profesión de Leon Kennedy en Resident Evil 2?",
+    respuesta: "Policía",
+    opciones: ["Bioquímico", "Alcalde", "Policía", "Mercenario"],
+  },
+  {
+    pregunta: "¿De quién es familiar Claire Redfile de la Saga Resident Evil?",
+    respuesta: "Chris",
+    opciones: ["Jill", "Chris", "Barry", "William"],
+  },
+  {
+    pregunta: "¿En qué continente toma parte la 5ta entrega de la Saga Resident Evil?",
+    respuesta: "Africa",
+    opciones: ["Antartida", "Europa", "Asia", "Africa"],
+  },
+  {
+    pregunta: "¿Cuáles son los enemigos principales de Resident Evil(4)?",
+    respuesta: "Plagas",
+    opciones: ["Zombies", "Cerberos", "Plagas", "Hongos"],
+  },
+
+  // FÚTBOL
+  {
+    pregunta: "¿Cuál es el equipo de fútbol con más copas Libertadores?",
+    respuesta: "Independiente",
+    opciones: ["Boca", "Independiente", "Flamengo", "Palmeiras"],
+  },
+  {
+     pregunta: "¿Cuál es el equipo de fútbol con más Champions?",
+    respuesta: "Real Madrid",
+    opciones: ["Bayern Munich", "Manchester United", "Real Madrid", "Barcelona"],
+  },
+  {
+    pregunta: "¿Cuál de estas selecciones NO ganó una copa del mundo?",
+    respuesta: "Paises Bajos",
+    opciones: ["Paises Bajos", "España", "Inglaterra", "Uruguay"],
+  },
+  {
+    pregunta: "¿En dónde se jugó la copa mundial de 1998?",
+    respuesta: "Francia",
+    opciones: ["Corea-Japón", "Bélgica", "Estados Unidos", "Francia"],
+  },
+  {
+    pregunta: "¿Cuál de estos jugadores NO jugo en PSG?",
+    respuesta: "Tevez",
+    opciones: ["Messi", "Tevez", "Cavani", "Gallardo"],
+  },
+  {
+    pregunta: "¿Cuál de estos equipos NUNCA descendió?",
+    respuesta: "Boca",
+    opciones: ["River", "Independiente", "Boca", "Racing"],
+  },
+  {
+    pregunta: "¿Que selección salió campeona en 2010?",
+    respuesta: "España",
+    opciones: ["Francia", "Alemania", "Brasil", "España"],
+  },
+  {
+    pregunta: "¿Cuál de estos equipos no es Italiano?",
+    respuesta: "Leipzing",
+    opciones: ["Leipzing", "Parma", "Inter", "Juventus"],
+  },
+  {
+    pregunta: "¿En qué año no salió campeón Argentina?",
+    respuesta: "1990",
+    opciones: ["1978", "1986", "1990", "2022"],
+  },
+  {
+    pregunta: "¿Cuantos mundiales tiene Brasil?",
+    respuesta: "5",
+    opciones: ["3", "4", "5", "6"],
+  },
+
+  // MUSICA
+  {
+    pregunta: "¿Cuál de estas canciones no es de Michael Jackson?",
+    respuesta: "Uptown Funk",
+    opciones: ["Uptown Funk", "Beat it", "Black or White", "Thriller"],
+  },
+  {
+    pregunta: "¿Cuál de estas canciones no es de Gorillaz?",
+    respuesta: "Love Generation",
+    opciones: ["19-2000", "Love Generation", "Stylo", "Feel Good Inc"],
+  },
+  {
+    pregunta: "¿Cuál de estas canciones no es de Soda Stereo?",
+    respuesta: "Al Atardecer",
+    opciones: ["Lo que Sangra", "Paseando por Roma", "Al Atardecer", "Entre Canibales"],
+  },
+  {
+    pregunta: "¿Cuál de estas canciones no es de SUMO?",
+    respuesta: "Ala Delta",
+    opciones: ["Next Week", "Los Viejos Vinagres", "Debede", "Ala Delta"],
+  },
+  {
+    pregunta: "¿Cuál de estas canciones no es de Dua Lipa?",
+    respuesta: "Espresso",
+    opciones: ["Espresso", "Levitating", "Physical", "Houdini"],
+  },
+  {
+    pregunta: "¿Cuál de estas canciones no es de Queen?",
+    respuesta: "November Rain",
+    opciones: ["Don't stop me now", "Killer Queen", "November Rain", "Love of my Life"],
+  },
+  {
+    pregunta: "¿Cuál de estas canciones no es de The Beatles?",
+    respuesta: "Stairway to Heaven",
+    opciones: ["Let It Be", "Hey Jude", "Stairway to Heaven", "Come Together"],
+  },
+  {
+    pregunta: "¿Cuál de estas canciones no es de Coldplay?",
+    respuesta: "Creep",
+    opciones: ["The Scientist", "Yellow", "Creep", "Fix You"],
+  },
+  {
+    pregunta: "¿Cuál de estas canciones no es de Patricio Rey y sus Redonditos de Ricota?",
+    respuesta: "La Mosca y la Sopa",
+    opciones: ["Jijiji", "La Mosca y la Sopa", "Popotitos", "Un Tal Pampita"],
+  },
+  {
+    pregunta: "¿Cuál de estas canciones no es de AC/DC?",
+    respuesta: "Smells Like Teen Spirit",
+    opciones: ["Highway to Hell", "Thunderstruck", "Smells Like Teen Spirit", "Back in Black"],
+  },
+  // CULTURA GENERAL ARGENTINA
+  {
+    pregunta: "¿Cuál de estos escritores NO es argentino?",
+    respuesta: "Mario Vargas Llosa",
+    opciones: ["Jorge Luis Borges", "Mario Vargas Llosa", "Julio Cortázar", "Roberto Arlt"],
+  },
+  {
+    pregunta: "¿Cómo se llama la moneda oficial de Argentina?",
+    respuesta: "Peso",
+    opciones: ["Real", "Peso", "Austral", "Patacón"],
+  },
+  {
+    pregunta: "¿Cuál es la provincia más grande de Argentina?",
     respuesta: "Buenos Aires",
-    opciones: ["Córdoba", "Rosario", "Buenos Aires", "Mendoza"],
+    opciones: ["Córdoba", "Santa Cruz", "Buenos Aires", "Chaco"],
   },
   {
-    pregunta: "¿Quién fue el general que cruzó los Andes para liberar a Chile?",
-    respuesta: "José de San Martín",
-    opciones: ["Manuel Belgrano", "José de San Martín", "Simón Bolívar", "Güemes"],
+    pregunta: "¿Cuál de estos no es un premio Nobel argentino?",
+    respuesta: "Jorge Luis Borges",
+    opciones: ["César Milstein", "Jorge Luis Borges", "Bernardo Houssay", "Luis Federico Leloir"],
   },
   {
-    pregunta: "¿En qué año comienza la Guerra de la Independencia en Argentina?",
-    respuesta: "1810",
-    opciones: ["1808", "1810", "1812", "1816"],
-  },
-
-  // GEOGRAFÍA DE ARGENTINA
-  {
-    pregunta: "¿Cuál es el río más importante de Argentina?",
-    respuesta: "Río de la Plata",
-    opciones: ["Paraná", "Río de la Plata", "Río Negro", "Bermejo"],
+    pregunta: "¿En qué año se produjo el Cordobazo?",
+    respuesta: "1969",
+    opciones: ["1955", "1969", "1973", "1976"],
   },
   {
-    pregunta: "¿En qué provincia se encuentran las Cataratas del Iguazú?",
-    respuesta: "Misiones",
-    opciones: ["Jujuy", "Corrientes", "Misiones", "Entre Ríos"],
+    pregunta: "¿Cuál de estas series NO es de producción argentina?",
+    respuesta: "Club de Cuervos",
+    opciones: ["El Marginal", "Okupas", "Club de Cuervos", "Tumberos"],
   },
   {
-    pregunta: "¿Cuántos continentes hay en el mundo?",
-    respuesta: "6",
-    opciones: ["5", "6", "7", "8"],
+    pregunta: "¿Cómo se llama el himno nacional argentino?",
+    respuesta: "Himno Nacional Argentino",
+    opciones: ["Marcha de la Patria", "Himno Nacional Argentino", "Canción Patria", "Aurora"],
   },
   {
-    pregunta: "¿Cuál es la montaña más alta de Argentina?",
-    respuesta: "Aconcagua",
-    opciones: ["Tupungato", "Mercedario", "Aconcagua", "Nevado de Ruiz"],
+    pregunta: "¿Cuál es el deporte nacional de Argentina?",
+    respuesta: "Pato",
+    opciones: ["Fútbol", "Rugby", "Polo", "Pato"],
   },
   {
-    pregunta: "¿En qué región se encuentra la ciudad de Mendoza?",
-    respuesta: "Cuyo",
-    opciones: ["Patagonia", "Cuyo", "Litoral", "Noroeste"],
-  },
-
-  // CULTURA ARGENTINA
-  {
-    pregunta: "¿Cuál es la bebida tradicional argentina que se comparte entre amigos?",
-    respuesta: "Mate",
-    opciones: ["Café", "Tereré", "Mate", "Té"],
+    pregunta: "¿Quién diseñó la bandera argentina?",
+    respuesta: "Manuel Belgrano",
+    opciones: ["José de San Martín", "Manuel Belgrano", "Mariano Moreno", "Bernardino Rivadavia"],
   },
   {
-    pregunta: "¿Quién fue el cantante legendario del tango argentino?",
-    respuesta: "Carlos Gardel",
-    opciones: ["Julio Sosa", "Carlos Gardel", "Astor Piazzolla", "Osvaldo Pugliese"],
-  },
-  {
-    pregunta: "¿Cuántos mundiales ganó Argentina en fútbol?",
-    respuesta: "3",
-    opciones: ["1", "2", "3", "4"],
-  },
-  {
-    pregunta: "¿Cuál es el baile tradicional de Argentina?",
-    respuesta: "Tango",
-    opciones: ["Salsa", "Tango", "Folclore", "Chacarera"],
-  },
-  {
-    pregunta: "¿Qué comida argentina es un corte de carne popular?",
-    respuesta: "Asado",
-    opciones: ["Empanada", "Milanesa", "Asado", "Choripán"],
-  },
-
-  // DEPORTES ARGENTINOS
-  {
-    pregunta: "¿Cuántas Copas América ha ganado Argentina?",
-    respuesta: "15",
-    opciones: ["12", "14", "15", "16"],
-  },
-  {
-    pregunta: "¿En qué año ganó Argentina su primer Mundial?",
-    respuesta: "1978",
-    opciones: ["1974", "1978", "1982", "1986"],
-  },
-
-  // DIVERSIÓN Y CURIOSIDADES
-  {
-    pregunta: "¿Cuál es el animal terrestre más rápido?",
-    respuesta: "Guepardo",
-    opciones: ["Leona", "Guepardo", "Caballo", "Gacela"],
-  },
-  {
-    pregunta: "¿Cuántas patas tiene una araña?",
-    respuesta: "8",
-    opciones: ["6", "8", "10", "12"],
-  },
-  {
-    pregunta: "¿En qué país se originó el chocolate?",
-    respuesta: "México",
-    opciones: ["Suiza", "Bélgica", "México", "Italia"],
-  },
-  {
-    pregunta: "¿Cuántas vidas tiene un gato según la leyenda?",
-    respuesta: "9",
-    opciones: ["5", "7", "9", "11"],
-  },
-  {
-    pregunta: "¿Cuál es el animal más grande del mundo?",
-    respuesta: "Ballena Azul",
-    opciones: ["Elefante", "Jirafa", "Ballena Azul", "Hipopótamo"],
-  },
-  {
-    pregunta: "¿Qué país inventó la pizza?",
-    respuesta: "Italia",
-    opciones: ["España", "Italia", "Francia", "Grecia"],
-  },
-  {
-    pregunta: "¿Cuántos colores tiene el arco iris?",
-    respuesta: "7",
-    opciones: ["5", "6", "7", "8"],
-  },
-  {
-    pregunta: "¿En qué continente viven los pingüinos?",
-    respuesta: "Antártida",
-    opciones: ["América del Sur", "África", "Antártida", "Australia"],
+    pregunta: "¿Cuál de estas provincias NO tiene salida al mar?",
+    respuesta: "Tucumán",
+    opciones: ["Buenos Aires", "Santa Cruz", "Chubut", "Tucumán"],
   },
 ];
 
